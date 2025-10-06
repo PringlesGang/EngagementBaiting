d3c03479cb9dd174425216536f83d3c0c635d071
J. Hebinck <j.hebinck@umail.leidenuniv.nl>
2025-10-06 16:04:53 +0200

    Cleaned up sentiment.py, prints both scores. Added feedback files with sentiment scores.

--- sentiment_analysis/sentiment.py
+++ sentiment_analysis/sentiment.py
@@ -1,20 +1,22 @@
 # https://huggingface.co/blog/sentiment-analysis-python
 
 from transformers import pipeline
+
 sentiment_pipeline = pipeline("sentiment-analysis")
-data = ["Try again!", "You're getting better", "Practice makes perfect", "Take your time", "I believe in you", "You can do this"]
+negative_feedback = ["haha get fucked", "you suck lol", "You are not a clown. You are the entire circus", "Were you even trying?",
+        "Touch grass", "You know dying isn't the goal, right?",
+        "Oh, sure, die again! See if I care", "Take your time",
+        "You're only wasting your own time y'know", "We've got alllll day",
+        "Lmao", "Another one bites the dust", "You died, what a surprise",
+        "Don't give up or do, I don't care either way"]
+positive_feedback = ["Try again!", "You're getting better", "Practice makes perfect", 
+        "Take your time", "I believe in you", "You can do this"] 
 
 
-print(sentiment_pipeline(data))
-
-# Try again!
-# You're getting better
-# Practice makes perfect
-# Take your time
-# I believe in you
-# You can do this
+print("Negative scores:", sentiment_pipeline(negative_feedback))
+print("Positive scores:", sentiment_pipeline(positive_feedback))
 
 
+# Negative scores: [{'label': 'NEGATIVE', 'score': 0.9992058873176575}, {'label': 'NEGATIVE', 'score': 0.9814907312393188}, {'label': 'POSITIVE', 'score': 0.9922102689743042}, {'label': 'NEGATIVE', 'score': 0.9965564012527466}, {'label': 'POSITIVE', 'score': 0.9991829991340637}, {'label': 'NEGATIVE', 'score': 0.9957679510116577}, {'label': 'NEGATIVE', 'score': 0.9872899651527405}, {'label': 'POSITIVE', 'score': 0.894005298614502}, {'label': 'NEGATIVE', 'score': 0.999669075012207}, {'label': 'POSITIVE', 'score': 0.8481776714324951}, {'label': 'POSITIVE', 'score': 0.8618462681770325}, {'label': 'NEGATIVE', 'score': 0.9975351095199585}, {'label': 'POSITIVE', 'score': 0.9941278696060181}, {'label': 'NEGATIVE', 'score': 0.9839351773262024}]
+# Positive scores: [{'label': 'POSITIVE', 'score': 0.7419360280036926}, {'label': 'POSITIVE', 'score': 0.9995009899139404}, {'label': 'POSITIVE', 'score': 0.9998564720153809}, {'label': 'POSITIVE', 'score': 0.894005298614502}, {'label': 'POSITIVE', 'score': 0.9997929930686951}, {'label': 'POSITIVE', 'score': 0.9997914433479309}]
 
-# Negative feedback: [{'label': 'NEGATIVE', 'score': 0.9992058873176575}, {'label': 'NEGATIVE', 'score': 0.9814907312393188}, {'label': 'POSITIVE', 'score': 0.9922102689743042}, {'label': 'NEGATIVE', 'score': 0.9965564012527466}, {'label': 'POSITIVE', 'score': 0.9991829991340637}, {'label': 'NEGATIVE', 'score': 0.9957679510116577}, {'label': 'NEGATIVE', 'score': 0.9872899651527405}, {'label': 'POSITIVE', 'score': 0.894005298614502}, {'label': 'NEGATIVE', 'score': 0.999669075012207}, {'label': 'POSITIVE', 'score': 0.8481776714324951}, {'label': 'POSITIVE', 'score': 0.8618462681770325}, {'label': 'NEGATIVE', 'score': 0.9975351095199585}, {'label': 'POSITIVE', 'score': 0.9941278696060181}, {'label': 'NEGATIVE', 'score': 0.9839351773262024}]
-# Positive feedback: [{'label': 'POSITIVE', 'score': 0.7419360280036926}, {'label': 'POSITIVE', 'score': 0.9995009899139404}, {'label': 'POSITIVE', 'score': 0.9998564720153809}, {'label': 'POSITIVE', 'score': 0.894005298614502}, {'label': 'POSITIVE', 'score': 0.9997929930686951}, {'label': 'POSITIVE', 'score': 0.9997914433479309}]
